a74064d40d048621505989de2c51be029a8655f0
Masaaki Morishita <ikasama3.0@gmail.com>
2014-12-09 20:49:24 +0900

    Implement Acss.process()

---
+++ index.js
@@ -0,0 +1,35 @@
+var postcss = require('postcss')
+var imprt = require('postcss-import')
+var constant = require('postcss-constant')
+var extend = require('postcss-extend')
+
+module.exports = Acss
+
+function Acss (css, options) {
+    if (!this instanceof Acss) return new Acss(css, options)
+    options = options || {}
+    this.css = css
+    this.compress = options.compress
+}
+
+Acss.prototype.process = function () {
+    var output
+    if (this.compres) {
+        output = postcss()
+                 .use(imprt())
+                 .use(constant(this.css))
+                 .use(extend(this.css))
+                 .use(csswring.postcss)
+                 .process(css)
+                 .css
+    }
+    else {
+        output = postcss()
+                 .use(imprt())
+                 .use(constant(this.css))
+                 .use(extend(this.css))
+                 .process(css)
+                 .css
+    }
+    return output
+}
